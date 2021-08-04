a7e6e945a9e9fbb993b3cad4508e892e7f9209d8
pzaqw4 <pzaqw4@gmail.com>
2021-08-04 22:38:58 +0800

    【重購】加入判斷是否登入的程式

---
+++ WebformMiniSample/AccountingNote.Auth/AuthManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace AccountingNote.Auth
+{
+    /// <summary>
+    /// 負責處理登入相關元件 (auth為登入驗證相關的名字)
+    /// </summary>
+    public class AuthManager
+    {
+        /// <summary>
+        /// 檢察目前是否登入
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsLogined()
+        {
+            if (HttpContext.Current.Session["UserLoginInfo"] == null)
+                return false;            
+            else
+                return true;
+        }
+    }
+}
--- WebformMiniSample/AccountingNote/SystemAdmin/AccountingList.aspx.cs
+++ WebformMiniSample/AccountingNote/SystemAdmin/AccountingList.aspx.cs
@@ -1,4 +1,5 @@
-﻿using AccountingNote.DBSource;
+﻿using AccountingNote.Auth;
+using AccountingNote.DBSource;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -15,7 +16,8 @@ namespace AccountingNote.SystemAdmin
         protected void Page_Load(object sender, EventArgs e)
         {
             //驗證登入
-            if (this.Session["UserLoginInfo"] == null)
+           //if (this.Session["UserLoginInfo"] == null)
+           if(!AuthManager.IsLogined())
             {
                 Response.Redirect("/Login.aspx");
                 return;
